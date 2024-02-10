b7e89d7b9439f3a48f00c743c8ac6aba070ca9fd
Paul Grimal <grimal-paul@outlook.fr>
2024-02-10 18:02:22 +0100

    feat: add forms to borrow tools

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,5 +1,9 @@
+import datetime
+
+from django import forms
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ModelForm
 
 
 # Create your models here.
@@ -26,10 +30,44 @@ class BorrowTool(models.Model):
 
     # a tool,
     # a user borrowing the tool,
-    # a date of borrowing,
+    # a date of borrowing, default to the current date
     # a number of hours of borrowing
     id = models.AutoField(primary_key=True)
     tool = models.ForeignKey(AgriculturalTool, on_delete=models.CASCADE, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    date_borrow = models.DateTimeField(auto_now_add=True)
-    time_borrow = models.IntegerField()
+    date_borrow = models.DateField()
+    time_borrow = models.TimeField()
+    comment = models.TextField(null=True)
+
+
+class BorrowToolForm(ModelForm):
+    class Meta:
+        model = BorrowTool
+        fields = ["tool", "user", "date_borrow", "time_borrow", "comment"]
+        widgets = {
+            "date_borrow": forms.DateInput(attrs={"type": "date", "value": datetime.date.today()}),
+            "time_borrow": forms.TimeInput(attrs={"type": "time", "value": "01:00"}),
+            "comment": forms.Textarea(attrs={"rows": 2, "cols": 50, "placeholder": "Commentaire"}),
+            "tool": forms.HiddenInput(),
+        }
+
+    # check the date cannot be in the futur
+    def clean_date_borrow(self):
+        date_borrow = self.cleaned_data["date_borrow"]
+        if date_borrow > datetime.date.today():
+            raise forms.ValidationError("La date ne peut pas être dans le futur")
+        return date_borrow
+
+    # id de l'utilisateur ne doit pas pouvoir être changé par l'utilisateur
+    # aller vers une page de confirmation qui demande de comfirmer le prêt et de vérifier les infos, on pourra le faire sur la même page avec du javascript
+
+
+# user ne doit pas pouvoir sélectionner autre user et sélectionner un autre outil
+class BorrowToolFormUser(BorrowToolForm):
+    # the tool must not be modifiable by the user
+    # and not appear in the form
+    # tool =  Field(widget=HiddenInput())
+    # hidden filed
+
+    class Meta(BorrowToolForm.Meta):
+        exclude = ["user"]  # exclude the user field
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
 
-from .models import AgriculturalTool
+from .models import AgriculturalTool, BorrowToolForm
 
 
 # Create your views here.
@@ -14,5 +16,54 @@ def index(request):
 
 
 def see_tool(request, tool_id):
-    tool = get_object_or_404(AgriculturalTool, pk=tool_id)
-    return render(request, "catalog/tool.html", {"tool": tool})
+    if request.method == "POST":
+        form = BorrowToolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # update the rendering
+            tool_id = form.cleaned_data["tool"].id
+            tool = get_object_or_404(AgriculturalTool, pk=tool_id)
+            user = User.objects.all()
+            form = BorrowToolForm(initial={"tool": tool})
+            messages.success(request, "L'utilisation de l'outil est bien enregistrée. Merci !")
+            return render(request, "catalog/tool.html", {"tool": tool, "users": user, "form": form})
+        else:
+            messages.error(request, "Il y a une erreur dans le formulaire. Merci de vérifier les informations.")
+            tool = get_object_or_404(AgriculturalTool, pk=tool_id)
+            user = User.objects.all()
+            form = BorrowToolForm(initial={"tool": tool})
+            return render(request, "catalog/tool.html", {"tool": tool, "users": user, "form": form})
+    else:
+        tool = get_object_or_404(AgriculturalTool, pk=tool_id)
+        # get all the user
+        user = User.objects.all()
+        # init form with the tool
+        form = BorrowToolForm(initial={"tool": tool})
+        return render(request, "catalog/tool.html", {"tool": tool, "users": user, "form": form})
+
+
+# def get_borrowed(request):
+#     """check if the form is valid and save it, the update the rendering of the page with a json message"""
+#     if request.method == "POST":
+#         pass
+
+
+# pré remplir le format du jour en le forcant
+
+# def get_borrowed(request):
+#     # avoir id du modele autrement que par le formulaire peut être
+#     if request.method == "POST":
+#         form = BorrowToolForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             # update the rendering
+#             tool_id = form.cleaned_data["tool"].id
+#             tool = get_object_or_404(AgriculturalTool, pk=tool_id)
+#             user = User.objects.all()
+#             see_tool(request, form.cleaned_data["tool"].id)
+#     else:
+#         return see_tool(request, form.cleaned_data["tool"].id)
+
+# pré remplir le format du jour en le forcant
+
+# @login_required
