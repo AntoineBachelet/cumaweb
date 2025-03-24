f0c9b715c8fbc640415dcc8e8e267429005cf16e
Antoine Bachelet <a.bachelet708@gmail.com>
2025-03-24 19:20:50 +0100

    fix: correct create url

--- src/users/urls.py
+++ src/users/urls.py
@@ -11,5 +11,5 @@ app_name = "users"
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path("create", createUser, name="createUser"),
+    path("create/", createUser, name="createUser"),
 ]
