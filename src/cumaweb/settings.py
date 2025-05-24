18dbadcfa30c0a214c6651827311d3adf10638d6
Antoine Bachelet <a.bachelet708@gmail.com>
2025-05-24 13:49:51 +0200

    fix: prepare env for production

--- src/cumaweb/settings.py
+++ src/cumaweb/settings.py
@@ -12,20 +12,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4cc54=9tlt6q)g$&s#z3hfti4x*w+rm=(*bynnrq%vdu@z!$00"
+# SECRET_KEY = "django-insecure-4cc54=9tlt6q)g$&s#z3hfti4x*w+rm=(*bynnrq%vdu@z!$00"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
